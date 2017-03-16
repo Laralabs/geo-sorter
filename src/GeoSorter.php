9190de9d57077e58e7c297f144a5ffb55d6e0d1d
Matt Clinton <matt@laralabs.uk>
2017-03-16 15:22:26 +0000

    geoSort() function added.
    Test and debug time!

--- src/GeoSorter.php
+++ src/GeoSorter.php
@@ -11,10 +11,12 @@
  * @author Matt Clinton <matt@laralabs.uk>
  */
 
-namespace App;
+namespace Laralabs\GeoSorter;
 
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Ayeo\Geo\Coordinate;
+use Ayeo\Geo\DistanceCalculator;
 
 class GeoSorter extends Collection
 {
@@ -30,12 +32,29 @@ class GeoSorter extends Collection
 
     public static function geoSort($collection, $postcode)
     {
-        $items = $collection;
-        $distanceArray = [];
+        $items          =   $collection;
+        $distanceArray  =   [];
+        $postcodeField  =   config('geosorter.postcodeField');
+        $sourceOutcode  =   GeoSorterPostcodes::where('area_code', '=', trim(substr(trim($postcode),0,-3)))->first();
+        $source         =   new Coordinate\Decimal($sourceOutcode->lat, $sourceOutcode->long);
 
         foreach($items as $item)
         {
+            $itemPostcode   =   $item->$postcodeField;
+            $outcode        =   GeoSorterPostcodes::where('area_code', '=', trim(substr(trim($itemPostcode),0,-3)))->first();
 
+            //Calculate the distance
+            $calculator     =   new DistanceCalculator();
+            $destination    =   new Coordinate\Decimal($outcode->lat, $outcode->long);
+            $distance       =   $calculator->getDistance($source, $destination);
+
+            $distanceItem   =   [
+                'id'        =>  $item->id,
+                'distance'  =>  $distance
+            ];
+            $distanceArray[]    =   $distanceItem;
         }
+
+        return $distanceArray;
     }
 }
