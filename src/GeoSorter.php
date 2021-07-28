3f764400a7d4f3fb0f9fd9cb525a30a931f4f202
Matt Clinton <matt@mattc.dev>
2021-07-28 18:27:05 +0100

    Refactor GeoSorter class

--- src/GeoSorter.php
+++ src/GeoSorter.php
@@ -16,37 +16,29 @@ use Laralabs\GeoSorter\Exceptions\InvalidPostcodeException;
 use Laralabs\GeoSorter\Exceptions\InvalidUnitException;
 use Laralabs\GeoSorter\Exceptions\PostcodeNotFoundException;
 use Laralabs\GeoSorter\Models\GeoSorterPostcodes;
+use Throwable;
 
 class GeoSorter
 {
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $field;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $sortOrder;
 
-    /**
-     * @var int
-     */
+    /** @var int */
     protected $radius;
 
-    /**
-     * @var string
-     */
+    /** @var string */
     protected $unit;
 
-    /**
-     * @var bool
-     */
+    /** @var bool */
     protected $strict;
 
-    /**
-     * @throws InvalidFieldException
-     */
+    /** @var DistanceCalculator */
+    protected $calculator;
+
+    /** @throws InvalidFieldException */
     public function __construct()
     {
         $this->field = config('geosorter.postcode_field');
@@ -54,10 +46,9 @@ class GeoSorter
         $this->radius = config('geosorter.distance_radius') ?? 0;
         $this->unit = config('geosorter.distance_unit') ?? 'miles';
         $this->strict = config('geosorter.strict_mode') ?? true;
+        $this->calculator = new DistanceCalculator();
 
-        if (!is_string($this->field)) {
-            throw new InvalidFieldException($this->field);
-        }
+        throw_if(is_string($this->field) === false, new InvalidFieldException($this->field));
     }
 
     /**
@@ -66,22 +57,16 @@ class GeoSorter
      * Sorts given collection based upon distance from the
      * given postcode, returns sorted collection.
      *
-     * @param Collection $collection
-     * @param string $postcode
-     * @param string|null $sort
-     * @return Collection|null
-     * @throws PostcodeNotFoundException|InvalidUnitException|InvalidPostcodeException
+     * @throws PostcodeNotFoundException|InvalidUnitException|InvalidPostcodeException|Throwable
      */
-    public function sortByPostcode($collection, $postcode, $sort = null): ?Collection
+    public function sortByPostcode(Collection $collection, string $postcode, ?string $sort = null): ?Collection
     {
-        if ($this->strict && preg_match('/^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/', $postcode) === 0) {
-            throw new InvalidPostcodeException($postcode);
-        }
+        $this->validatePostcode($postcode);
 
         /*
          * Calculate distance radius in metres
          */
-        if($this->radius > 0){
+        if ($this->radius > 0) {
             switch ($this->unit) {
                 case 'metres':
                     //Metres is what we want, do nothing
@@ -94,30 +79,21 @@ class GeoSorter
                     break;
                 default:
                     throw new InvalidUnitException($this->unit);
-                    break;
             }
         }
 
         /*
          * Make postcode uppercase and remove spaces
          */
-        $postcode = str_replace(' ', '', strtoupper($postcode));
+        $postcode = $this->formatPostcode($postcode);
 
         /*
          * Check length of postcode to see if we are just dealing with
          * an outcode.
          */
-        $length = strlen($postcode);
+        $sourceOutcode = $this->getOutcode($postcode);
 
-        if($length > 4) {
-            $sourceOutcode = GeoSorterPostcodes::where('area_code', '=', trim(substr(trim($postcode),0,-3)))->first();
-        }else{
-            $sourceOutcode = GeoSorterPostcodes::where('area_code', '=', $postcode)->first();
-        }
-
-        if (!$sourceOutcode instanceof GeoSorterPostcodes) {
-            throw new PostcodeNotFoundException();
-        }
+        throw_if($sourceOutcode instanceof GeoSorterPostcodes === false, new PostcodeNotFoundException());
 
         /*
          * Create Decimal object from source postcode latitude and longitude
@@ -130,51 +106,40 @@ class GeoSorter
          * source, map and populate 'distance' property on collection item.
          */
         $mapped = $collection->map(function ($item) use ($source) {
-
             $field = $this->field;
             $itemPostcode = $item->$field;
-            
-            if ($this->strict && preg_match('/^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/', $itemPostcode) === 0) {
-                throw new InvalidPostcodeException($itemPostcode);
-            }
+
+            $this->validatePostcode($itemPostcode);
 
             /*
              * Make postcode uppercase and remove spaces
              */
-            $itemPostcode = str_replace(' ', '', strtoupper($itemPostcode));
+            $itemPostcode = $this->formatPostcode($itemPostcode);
 
             /*
              * Check length of postcode to see if we are just dealing with
              * an outcode.
              */
-            $length         =   strlen($itemPostcode);
-            if ($length > 4) {
-                $outcode    =   GeoSorterPostcodes::where('area_code', '=', trim(substr(trim($itemPostcode), 0, -3)))->first();
-            } else {
-                $outcode    =   GeoSorterPostcodes::where('area_code', '=', $itemPostcode)->first();
-            }
+            $outcode = $this->getOutcode($itemPostcode);
 
             if ($outcode !== null && $source instanceof Decimal) {
                 /*
                  * Calculate the distance in metres
                  */
-                $calculator = new DistanceCalculator();
                 $destination = new Decimal($outcode->lat, $outcode->long);
-                $distance = $calculator->getDistance($source, $destination);
-                $item['distance'] = $distance;
+                $item['distance'] = $this->calculator->getDistance($source, $destination);
             } else {
                 $item['distance'] = -1;
             }
 
             return $item;
-
         });
 
         /*
          * Apply radius filter if it is set
          */
-        if($this->radius > 0) {
-            $mapped = $mapped->filter(function($item) {
+        if ($this->radius > 0) {
+            $mapped = $mapped->filter(function ($item): bool {
                 return $item->distance <= $this->radius;
             })->values();
         }
@@ -186,8 +151,32 @@ class GeoSorter
             $this->sortOrder = $sort;
         }
 
-        $mapped = $this->sortOrder === 'DESC' ? $mapped->sortByDesc('distance') : $mapped->sortBy('distance');
+        return $this->sortOrder === 'DESC' ? $mapped->sortByDesc('distance') : $mapped->sortBy('distance');
+    }
 
-        return $mapped;
+    private function formatPostcode(string $postcode): string
+    {
+        return str_replace(' ', '', strtoupper($postcode));
+    }
+
+    private function getOutcode(string $postcode): GeoSorterPostcodes
+    {
+        if (strlen($postcode) > 4) {
+            return GeoSorterPostcodes::where('area_code', '=', trim(substr(trim($postcode), 0, -3)))
+                ->first();
+        }
+
+        return GeoSorterPostcodes::where('area_code', '=', $postcode)->first();
+    }
+
+    private function validatePostcode(string $postcode): void
+    {
+        throw_if(
+            $this->strict && preg_match(
+                '/^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/',
+                $postcode
+            ) === 0,
+            new InvalidPostcodeException($postcode)
+        );
     }
 }
--- src/GeoSorterServiceProvider.php
+++ src/GeoSorterServiceProvider.php
@@ -13,37 +13,27 @@ use Laralabs\GeoSorter\Commands\UpdatePostcodes;
 
 class GeoSorterServiceProvider extends ServiceProvider
 {
-    /**
-     * Register the application services.
-     *
-     * @return void
-     */
-    public function register()
+    public function register(): void
     {
         $this->mergeConfigFrom(
-            __DIR__.'/../config/geosorter.php', 'geosorter'
+            __DIR__ . '/../config/geosorter.php', 'geosorter'
         );
 
         $this->commands([
             UpdatePostcodes::class
         ]);
 
-        $this->app->bind('geosorter', function ($app) {
+        $this->app->bind('geosorter', function (): GeoSorter {
             return new GeoSorter();
         });
     }
 
-    /**
-     * Bootstrap the application services.
-     *
-     * @return void
-     */
-    public function boot()
+    public function boot(): void
     {
         $this->publishes([
-            __DIR__.'/../config/geosorter.php'  => config_path('geosorter.php'),
+            __DIR__ . '/../config/geosorter.php'  => config_path('geosorter.php'),
         ], 'geosorter-config');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 }
